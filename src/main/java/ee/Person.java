cb4d60ea3b2169137e88285e6b8e0b0b20604e7b
guoweiwen <guowenen_wei@163.com>
2018-06-15 11:39:33 +0800

    修改打印条件

--- src/main/java/ee/Person.java
+++ src/main/java/ee/Person.java
@@ -6,7 +6,8 @@ public class Person {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("***" + StringUtils.isNumeric("***"));
+		  
+		System.out.println("***发送到发送到实时" + StringUtils.isNumeric("***"));
 	}
 
 }
